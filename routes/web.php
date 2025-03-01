35ae0069756b2fc83acfd0005f913f83b76fcfe5
ernieJohn <erniejohndbernabat@gmail.com>
2025-02-28 16:29:41 -0800

    add middleware to home route

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::view('/home', 'layouts.home');
+Route::view('/home', 'layouts.home')->middleware('auth');
